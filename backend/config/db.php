745548bf93048be77d69c56ae3dbe0fb850f5721
WangshuXC <1294673511@qq.com>
2023-12-19 22:00:41 +0800

    Update Backend

--- backend/config/db.php
+++ backend/config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=internetdatabasedevelopment',
     'username' => 'root',
-    'password' => '',
+    'password' => 'root',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
