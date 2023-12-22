688fd5f18194695f106add6e45457ce93e75e453
fxingan <2450521935@qq.com>
2023-12-22 19:25:02 +0800

    Update 新.sql

--- 新.sql
+++ 新.sql
@@ -242,7 +242,8 @@ DROP TABLE IF EXISTS `webviews`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `webviews` (
-  `Views` int DEFAULT '0'
+  `Views` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`Views`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -264,4 +265,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-22 18:59:51
+-- Dump completed on 2023-12-22 19:24:54
